e53386baa246ca35609d51153b8ca3a8c88bffe3
MAGANER <maganer.krik@mail.ru>
2019-07-21 11:55:44 +0300

    added more explaining commments

--- main.cpp
+++ main.cpp
@@ -19,16 +19,19 @@ int main()
 			}
 		}
 
+		//if key isn't pressed before, than console can be called 
 		if (Keyboard::isKeyPressed(Keyboard::Tilde) && !key_pressed)
 		{
 			bool is_working = cmdl.is_working();
 			if (is_working)
 			{
+				//if it's already running, set it off and clear
 				cmdl.set_working_state(false);
 				cmdl.clear();
 			}
 			else
 			{
+				//if it's not running, set it on 
 				cmdl.set_working_state(true);
 			}
 
@@ -38,11 +41,10 @@ int main()
 		{
 			key_pressed = false;
 		}
-
 		cmdl.get_input();
 
 		window.clear();
-		cmdl.draw(&window);
+		cmdl.draw(&window); // draw if it's working
 		window.display();
 	}
 
