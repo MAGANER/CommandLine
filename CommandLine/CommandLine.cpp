0a3263f70a3f08db61dd6cdd9903393ef3221993
MAGANER <maganer.krik@mail.ru>
2019-07-21 11:50:20 +0300

    added ability to set working state on/off

--- CommandLine/CommandLine.cpp
+++ CommandLine/CommandLine.cpp
@@ -1,8 +1,8 @@
 #include "CommandLine.h"
 CommandLine::CommandLine()
 {
+	//default parameters
 	font.loadFromFile("font/font.ttf");
-
 	entered_command = new Text("", font, 30);
 
 	key_pressed = false;
@@ -14,25 +14,29 @@ CommandLine::~CommandLine()
 
 void CommandLine::get_input()
 {
-	int first_key = Keyboard::A;
-	int last_key  = Keyboard::Num9;
-	int first_number = Keyboard::Num0;
-
-	for (int key = first_key; key < last_key; ++key)
+	if (working)
 	{
-		Keyboard::Key pressed_key = static_cast<Keyboard::Key>(key);
-		if (Keyboard::isKeyPressed(pressed_key) && !key_pressed)
-		{
-			//check is it char, if it's not, than it's number
-			bool is_char  = key < first_number;
-			if (is_char)
-			{
-				// why +65?
+	    int first_key = Keyboard::A;
+	    int last_key  = Keyboard::Num9;
+	    int first_number = Keyboard::Num0;
+
+	    for (int key = first_key; key < last_key; ++key)
+	    {
+		    Keyboard::Key pressed_key = static_cast<Keyboard::Key>(key);
+		    if (Keyboard::isKeyPressed(pressed_key) && !key_pressed)
+		    {
+			    //check is it char, if it's not, than it's number
+			    bool is_char  = key < first_number;
+			    if (is_char)
+			    {
+				   // why +65?
 				/*
 				  1) A symbol is element of enum Keyboard::Key
 					 and A is 0.
 				  2) if we convert char variable with A value into int
 					 we get variable with value 65, if B, than 66 e.t.c
+				  3) so 65 is "distance" between Key code from enumaration 
+				     of Keyboard::Key and ASCII symbols
 				*/
 				int _char = key + 65;
 
@@ -40,34 +44,46 @@ void CommandLine::get_input()
 				entered_text += entered_symbol;
 			}
 			else
-			{
-				
+			{		
+				//why +22
+			   /*
+				 the same reason as for characters.
+				 1) 0 is 26 in Keyboard::Key
+				 2) 0 is 48 in ASCII
+				 3) so 48-26 = 22  the distance between them 
+			   */
+
 				int _char = key + 22;
 				char entered_symbol = static_cast<char>(_char);
 				entered_text += entered_symbol;
 			}
+
 			key_pressed = true;
 		}
-		if (no_pressing())
-		{
-			key_pressed = false;
-		}
-			
+		    if (no_pressing())
+		    {
+			    //can press, if key isn't pressede already
+			    key_pressed = false;
+		    }
+	    }
 
+	    entered_command->setString(">"+entered_text);
 	}
-	entered_command->setString(entered_text);
+
 }
 void CommandLine::draw(RenderWindow* window)
 {
-	window->draw(*entered_command);
+  if(working) window->draw(*entered_command);
 }
 bool CommandLine::no_pressing()
 {
+	//array,containing states of all keys
 	bool pressed_keys_states[35];
 
 	int first_key = Keyboard::A;
 	int last_key = Keyboard::Num9;
 
+	// check every key, is it pressed
 	for (int key = first_key; key < last_key; ++key)
 	{
 		Keyboard::Key pressed_key = static_cast<Keyboard::Key>(key);
@@ -75,6 +91,7 @@ bool CommandLine::no_pressing()
 		pressed_keys_states[key] = is_pressing;
 	}
 
+	// if there is no pressed keys, than key isn't pressed, obviosly
 	for (int i = 0; i < 35; ++i)
 	{
 		if (pressed_keys_states[i])
@@ -83,4 +100,12 @@ bool CommandLine::no_pressing()
 		}
 	}
 	return true;
+}
+void CommandLine::set_working_state(bool flag)
+{
+	working = flag;
+}
+bool CommandLine::is_working()
+{
+	return working;
 }
--- CommandLine/CommandLine.h
+++ CommandLine/CommandLine.h
@@ -1,25 +1,36 @@
 #pragma once
 #include"SFML\Graphics.hpp"
+#include<string>
+using namespace std;
 using namespace sf;
 
-#include<iostream>
-using namespace std;
-
+/*
+ the base class for CommandLine that can be used
+ as integrated Console in SFML applications.
+ This class provides ability to get input only.
+ So it means you have to write function to check input
+ and apply action
+    - MAGANER
+*/
 class CommandLine
 {
 private:
-	Font font;
-	Text* entered_command;
+	Font font;             //can be changed
+	Text* entered_command; //rendered command text
 
-	string entered_text;
+	string entered_text;   //input string
 
-	bool key_pressed;
+	bool key_pressed;      //is used to prevent key repeating
+	bool working;          // working = true, not working = false
 public:
 	CommandLine();
 	virtual ~CommandLine();
 
 	void get_input();
 	void draw(RenderWindow* window);
+
+	void set_working_state(bool flag);
+	bool is_working();
 private:
 	bool no_pressing();
 };
